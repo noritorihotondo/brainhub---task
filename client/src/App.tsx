5d804c7edc5dbd53585d12c9526d158ec439c629
Tomdom <noritorihotondo@wp.pl>
2022-09-19 20:00:37 +0200

    Add rooting and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { paths } from './config/url';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      elo
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {paths.map((x, index) => (
+          <Route path={x.path} element={x.component()} key={index + x.path} />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
