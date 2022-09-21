543b216df6db58eb2bf99b6e92ded0818b457bc9
Tomdom <noritorihotondo@wp.pl>
2022-09-21 19:14:03 +0200

    Create Event

--- client/src/views/CreateEvent.tsx
+++ client/src/views/CreateEvent.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import {
+  MainWrapper,
+  Typography,
+  FormControl,
+  Input,
+  Button,
+  InputWrapper,
+  DateInput,
+} from '../components';
+import { CreateEventSchema } from '../types/Event';
 
 export const CreateEvent = () => {
-  return <h1>Create Event</h1>;
+  return (
+    <MainWrapper>
+      <Typography variant="h2" text="Create Event"></Typography>
+      <FormControl submitHandler={(data) => console.log(data)} validationSchema={CreateEventSchema}>
+        <InputWrapper>
+          <Input name="name" label="Event Name" placeholder="Birthday" />
+          <DateInput name="date" />
+        </InputWrapper>
+        <Button variant="text" text="Submit" type="submit"></Button>
+      </FormControl>
+    </MainWrapper>
+  );
 };
