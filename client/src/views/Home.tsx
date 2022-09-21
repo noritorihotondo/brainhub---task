df4223c66fd3f210177dac3cc2d23259bf655439
Tomdom <noritorihotondo@wp.pl>
2022-09-21 12:17:33 +0200

    Home Page

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import styled from 'styled-components';
+import { Box } from '@mui/material';
+import { Link, Typography } from '../components';
+
+const MainWrapper = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const LinksWrapper = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 20px;
+  gap: 30px;
+`;
 
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <Button variant="contained">elo</Button>
+      <MainWrapper>
+        <Typography variant="h2" text="Home"></Typography>
+        <LinksWrapper>
+          <Link to="/create-user" text="Create User" />
+          <Link to="/create-event" text="Create Event" />
+        </LinksWrapper>
+      </MainWrapper>
     </>
   );
 };
